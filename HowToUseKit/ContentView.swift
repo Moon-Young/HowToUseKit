48522ff27a6012c0706af7d895b25e95741865bf
Moon_Young <oooo5299@gmail.com>
2026-03-06 11:31:12 +0900

    Initial Commit

---
+++ HowToUseKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HowToUseKit
+//
+//  Created by Moonyoung on 3/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HowToUseKit/HowToUseKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseKitApp.swift
+//  HowToUseKit
+//
+//  Created by Moonyoung on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
